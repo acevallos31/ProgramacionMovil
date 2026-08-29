8a0dff4d263a08854d3a135f58ac789e2be9252b
Axeell Cevallos <acevallos31@gmail.com>
2026-08-29 16:00:10 -0600

    feat: integrar ThemeProvider en la aplicacion

--- MiAppTemas/App.tsx
+++ MiAppTemas/App.tsx
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider, useTheme } from './src/context/ThemeContext';
+
+function MainContent() {
+  const { colors } = useTheme();
+
+  return (
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <Text style={{ color: colors.text }}>
+        Laboratorio Temas, Contextos y Componentes
+      </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <MainContent />
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
