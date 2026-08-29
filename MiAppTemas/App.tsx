9b6786186b9c77b208e0fb1399a37188afbce66e
Axeell Cevallos <acevallos31@gmail.com>
2026-08-29 16:14:03 -0600

    feat: implementar pantallas y tab navigator

--- MiAppTemas/App.tsx
+++ MiAppTemas/App.tsx
@@ -1,33 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { ThemeProvider, useTheme } from './src/context/ThemeContext';
-
-function MainContent() {
-  const { colors } = useTheme();
-
-  return (
-    <View style={[styles.container, { backgroundColor: colors.background }]}>
-      <Text style={{ color: colors.text }}>
-        Laboratorio Temas, Contextos y Componentes
-      </Text>
-
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from './src/context/ThemeContext';
+import TabsNavigator from './src/navigation/TabsNavigator';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <MainContent />
+      <NavigationContainer>
+        <TabsNavigator />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
